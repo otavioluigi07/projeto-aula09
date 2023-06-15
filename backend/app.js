023a63cf7f16ce75a2fa637d6784dbfad4cbd03d
otavioluigi07 <Luigi.Macedo@sou.inteli.edu.br>
2023-06-15 09:56:46 -0300

    aula09_LuigiOtavio

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express()
+const hostname = '127.0.0.1';
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = './data/recebe-dados.db';
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var db = new sqlite3.Database(DBPATH);
+app.use(express.static('./frontend'));
+
+const path = require('path');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
+});
+
+app.get('/get',(req, res) =>{
+    res.setHeader('Acess-Control-Allow-Origin', '*');
+    var sql = 'SELECT * FROM program'
+    db.all(sql,[],(err,rows)=>{
+        if (err){
+            res.send(err)
+        }
+        res.send(rows)
+    })
+
+});
+
+app.post('/mensagem', (req, res) => {
+    console.log("passei aqui");
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    sql = "INSERT INTO program (mensagem) VALUES (?)";
+    console.log(sql);
+    db.run(sql, [req.body.data],  err => {
+        if (err) {
+            throw err;
+        }
+    });
+    res.write('<p>USUARIO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+    db.close(); // Fecha o banco
+    res.end();
+});
+  
+  
+app.listen(port, hostname, () => {
+console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
+
+
+
+
+
+
+
+
---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+
+function submeter() {
+document.getElementById('form').addEventListener("submit", function(event){
+    event.preventDefault()
+
+    var data = document.getElementById('mensagem')
+    var jj = data.value
+    console.log(jj)
+    var json_data = JSON.stringify({data: jj})
+    
+
+    const url = '/mensagem';    
+
+    fetch(url, {
+        method: 'POST',
+        body: json_data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        console.log("Inserido com sucesso!")
+    })
+
+
+})
+
+
+
+
+    
+}
